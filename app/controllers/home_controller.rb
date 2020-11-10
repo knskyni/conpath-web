eebaa1d6ec6175a1c8cd1334352c4cdd09a4d6d4
knskyni <knskyni@yanai-k.com>
2020-11-10 10:49:51 +0900

    change: サブヘッダーをapplication.html.erbに移動した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
+    @sub_header = {
+        title: "General",
+        list: [
+            {
+                name: "General",
+                url: "/"
+            },
+            {
+                name: "Empty Page",
+                url: "/test"
+            }
+        ]
+    }
   end
 end
