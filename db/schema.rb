03aa3ed8987023bbbb2f89f25ec2907589a11a37
knskyni <knskyni@yanai-k.com>
2020-11-17 01:06:58 +0900

    feat: Courseモデルの作成&マイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_154133) do
+ActiveRecord::Schema.define(version: 2020_11_16_154410) do
+
+  create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "school_id"
+    t.string "name"
+    t.integer "number_of_enrolled_year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "schools", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
