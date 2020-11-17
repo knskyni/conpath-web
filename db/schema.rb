036abdf9b88a512ddf646792bc89f21258309719
knskyni <knskyni@yanai-k.com>
2020-11-17 11:36:05 +0900

    feat: RecruitCompanyTagモデルを作成&マイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_023207) do
+ActiveRecord::Schema.define(version: 2020_11_17_023458) do
 
   create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "school_id"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 2020_11_17_023207) do
   end
 
   create_table "recruit_company_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recruit_company_tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
