29fab2d479e11c4c5b5cf24711c655c81d1fcc94
yanakence <yanakence@yanah.jp>
2020-11-17 11:22:47 +0900

    feat: Prefectureモデルを作成&マイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_014051) do
+ActiveRecord::Schema.define(version: 2020_11_17_021930) do
 
   create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "school_id"
     t.string "name"
     t.integer "number_of_enrolled_year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "name_furigana"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
