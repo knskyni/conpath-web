d526d4c1e3233051e0b65c4f00939e26281e93c7
yanakence <yanakence@yanah.jp>
2020-11-17 11:15:46 +0900

    change: studentsテーブルのidカラムとintからvarcharに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_012111) do
+ActiveRecord::Schema.define(version: 2020_11_17_014051) do
 
   create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "school_id"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_012111) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "students", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "students", id: :string, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email"
     t.string "password"
     t.string "last_name"
