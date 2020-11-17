73cf05985114403d60b069b5a974bee0c7e950c2
yanakence <yanakence@yanah.jp>
2020-11-18 03:42:32 +0900

    feat: 学校一覧を実装

--- app/controllers/school_controller.rb
+++ app/controllers/school_controller.rb
@@ -1,5 +1,16 @@
 class SchoolController < ApplicationController
   def index
-    
+    @sub_header = {
+        title: "学校一覧",
+        list: [
+            {
+                name: "学校",
+                url: "/school"
+            }
+        ]
+    }
+
+    # 学校一覧取得
+    @schools = School.all
   end
 end
