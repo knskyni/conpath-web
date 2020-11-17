2d93e5febe24212f764c8ffd8edcbdb05e2b2647
yanakence <yanakence@yanah.jp>
2020-11-17 15:46:49 +0900

    feat: ログイン中のユーザーを格納する@current_userを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def authenticate
+    if session[:user_type]
+      if session[:user_type] == :student
+        @current_user = Student.find_by(id: session[:user_id])
+      elsif session[:user_type] == :teacher
+        @current_user = Teacher.find_by(id: session[:user_id])
+      end
+    end
+  end
 end
