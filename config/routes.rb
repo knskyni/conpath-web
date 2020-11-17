f524f3559050a9511ed6234faff57b60ed90228d
haya <tontomingz@gmail.com>
2020-11-17 16:12:44 +0900

    change: URL変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'student/createform'
+  get 'student/create', to: 'student#create_form'
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
