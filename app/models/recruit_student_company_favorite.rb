26a127c362b56886f4e5fa3f5c03ba41ca7e1348
knskyni <knskyni@yanai-k.com>
2020-11-17 11:53:45 +0900

    feat: RecruitStudentCompanyFavoriteモデルを作成&マイグレート

---
+++ app/models/recruit_student_company_favorite.rb
@@ -0,0 +1,2 @@
+class RecruitStudentCompanyFavorite < ApplicationRecord
+end
---
+++ test/models/recruit_student_company_favorite_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RecruitStudentCompanyFavoriteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
