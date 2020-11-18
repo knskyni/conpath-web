4b032bff59de9363e5af30495ecdcd39dce5fdd4
ikeguchi <ikenana619@gmail.com>
2020-11-18 16:47:13 +0900

    feat: Teacher登録機能を追加

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -1,5 +1,18 @@
 class TeacherController < ApplicationController
   def new
-
   end
+
+  def create
+    @teacher = Teacher.new(lastName: params[:lastName])
+    @teacher = Teacher.new(params[:firstName])
+    @teacher = Teacher.new(params[:lastNameFurigana])
+    @teacher = Teacher.new(params[:firstNameFurigana])
+    @teacher = Teacher.new(params[:email])
+    @teacher = Teacher.new(params[:password])
+    @teacher = Teacher.new(params[:gender])
+    @teacher = Teacher.new(params[:biography])
+    @teacher.save
+    redirect_to("/teacher/")
+  end
+
 end
