365967e44869ca4f1d9a5702f393dc6fefc89d7d
yanakence <yanakence@yanah.jp>
2020-11-18 11:53:19 +0900

    feat: 学校詳細ページの左カラムを実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   get 'school', to: 'school#index'
+  get 'school/:id', to: 'school#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
