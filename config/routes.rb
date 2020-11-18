ffb005da72b18d8903abf2f8e9af4318479cb74f
ikeguchi <ikenana619@gmail.com>
2020-11-18 16:47:13 +0900

    feat: Teacher登録機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get 'teacher/new', to: 'teacher#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'teacher/create', to 'teacher#create'
 end
