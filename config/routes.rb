4dd02a6f7062bbb5a78dbde65dc955d12696f831
haya <tontomingz@gmail.com>
2020-11-18 16:46:18 +0900

    feat: 学生登録機能の実装(仮)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'student/create', to: 'student#create_form'
+  post 'student/create', to: 'student#create'
+
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
