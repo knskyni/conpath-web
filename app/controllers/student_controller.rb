8477e4b748b97792b00d6ee93c936bccad7c66e0
haya <tontomingz@gmail.com>
2020-11-18 16:46:18 +0900

    feat: 学生登録機能の実装(仮)

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,4 +1,43 @@
 class StudentController < ApplicationController
   def create_form
+    @sub_header = {
+        title: "General",
+        list: [
+            {
+                name: "General",
+                url: "/"
+            },
+            {
+                name: "Empty Page",
+                url: "/test"
+            }
+        ]
+    }
+    @student = Student.new
+  end
+
+  def create_form
+    @student = Student.new(
+        id: params[:id],
+        email: params[:email],
+        password: params[:password],
+        lastName: params[:last_name],
+        firstName: params[:first_name],
+        lastNameFurigana: params[:last_name_furigana],
+        firstNameFurigana: params[:first_name_furigana],
+        classId: params[:class_id],
+        gender: params[:gender],
+        icon: "default_user.jpg"
+    )
+    if @student.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/student/create")
+    elsif params[:password] == params[:password_confirm]
+      @error_message = "パスワードとパスワード確認が違います"
+      render("student/create")
+    else
+      @error_message = ""
+      render("student/create")
+    end
   end
 end
