3794e791722314fd350bd1674aeea9efdd0ab6fb
knskyni <knskyni@yanai-k.com>
2020-11-20 11:33:23 +0900

    feat: 学校更新のアクション作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'school/new', to: 'school#new'
   post 'school/create', to: 'school#create'
   get 'school/:id', to: 'school#show'
+  get 'school/:id/edit', to: 'school#edit'
+  post 'school/:id/update', to: 'school#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
