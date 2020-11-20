d7621c6a493b158c36b043f6a34485b7241050ed
yanakence <yanakence@yanah.jp>
2020-11-20 11:49:28 +0900

    feat: 学校名に文字数バリデーションを実装

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,5 @@
 class School < ApplicationRecord
-  validates :name, {presence: true}
+  validates :name, {presence: true, length: {maximum: 255}}
 
   def courses
     return Course.where(school_id: self.id)
