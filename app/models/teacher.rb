d9611bf41d637d4d839b2392a5818e362cb3df70
ikeguchi <ikenana619@gmail.com>
2020-11-20 10:42:51 +0900

    feat: Teacher/new内の変数名を変更

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,10 +1,11 @@
 class Teacher < ApplicationRecord
   has_secure_password
 
-  validates :email, {presence: true}
-  validates :last_name, {presence: true}
-  validates :first_name, {presence: true}
-  validates :last_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}}
-  validates :first_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}}
+  validates :email, {presence: true, length: [maximum: 255]}
+  validates :last_name, {presence: true, length: [maximum: 255]}
+  validates :first_name, {presence: true,length: [maximum: 255]}
+  validates :last_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}, length: [maximum: 255]}
+  validates :first_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}, length: [maximum: 255]}
   validates :gender, {presence: true}
+  validates :biography, {presence: true, length: [maximum: 255]}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get 'teacher/new', to: 'teacher#new'
-  post 'teacher/create', to 'teacher#create'
+  post 'teacher/create', to: 'teacher#create'
 end
