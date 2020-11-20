9f77b5f7796e93644f099d808a5fb915c24eaba0
haya <tontomingz@gmail.com>
2020-11-20 12:11:32 +0900

    feat: 学生情報修正機能実装

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,13 +1,15 @@
 class Student < ApplicationRecord
   has_secure_password
 
-  validates :email, {presence: true}
-  validates :last_name, {presence: true}
-  validates :first_name, {presence: true}
-  validates :last_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}}
-  validates :first_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}}
+  validates :email, {presence: true}, length: {maximum: 128}
+  validates :last_name, {presence: true}, length: {maximum: 64}
+  validates :first_name, {presence: true}, length: {maximum: 64}
+  validates :last_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}}, length: {maximum: 64}
+  validates :first_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}}, length: {maximum: 64}
   validates :class_id, {presence: true}
   validates :gender, {presence: true}
+  validates :biography, length: {maximum: 1024}
+  validates :password, length: {maximum: 64 }
 
   def student_class
     return SchoolClass.find_by(id: self.class_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'student/new', to: 'student#new'
   post 'student/create', to: 'student#create'
+  get 'student/edit/:id', to: 'student#edit'
+  post 'student/update/:id', to: 'student#update'
 
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
