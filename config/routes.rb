399826abef24d568b6aee8ff30933b25075131a8
yanakence <yanakence@yanah.jp>
2020-11-20 11:41:50 +0900

    feat: 学校情報更新処理を実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   post 'school/create', to: 'school#create'
   get 'school/:id', to: 'school#show'
   get 'school/:id/edit', to: 'school#edit'
-  post 'school/:id/update', to: 'school#update'
+  patch 'school/:id/update', to: 'school#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
