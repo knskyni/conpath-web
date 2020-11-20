b74345e4ab0df9fb99b63913a7ac031ff4a47e4e
haya <tontomingz@gmail.com>
2020-11-20 12:11:32 +0900

    feat: 学生情報修正機能実装

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -36,4 +36,30 @@ class StudentController < ApplicationController
       render("student/new")
     end
   end
+
+  def edit
+    @student = Student.find_by(id: params[:id])
+  end
+
+  def update
+    @student = Student.find_by(id: params[:id])
+    @student.email = params[:email]
+    @student.last_name = params[:last_name]
+    @student.first_name = params[:first_name]
+    @student.last_name_furigana = params[:last_name_furigana]
+    @student.first_name_furigana = params[:first_name_furigana]
+    @student.gender = params[:gender]
+    @student.biography = params[:biography]
+    if params[:icon]
+      @student.icon = "#{@student.id}.jpg"
+      image = params[:icon]
+      File.binwrite("public/student_icons/#{@student.icon}", image.read)
+    end
+    if @student.save
+      flash[:notice] = "ユーザー情報変更完了しました"
+      redirect_to("/student/edit/#{@student.id}")
+    else
+      render("student/edit")
+    end
+  end
 end
