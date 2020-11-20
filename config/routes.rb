672c583eb5c1bebed85fdac729b02793df536855
knskyni <knskyni@yanai-k.com>
2020-11-20 11:30:08 +0900

    change: 学校登録処理のURLを変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'school', to: 'school#index'
   get 'school/new', to: 'school#new'
-  post 'school/new', to: 'school#create'
+  post 'school/create', to: 'school#create'
   get 'school/:id', to: 'school#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
