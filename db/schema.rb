78f98acc827cb2ffcdc75740565aae0e6db2149b
knskyni <knskyni@yanai-k.com>
2020-11-22 21:28:55 +0900

    feat: school_createテーブルにauth_keyカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_121442) do
+ActiveRecord::Schema.define(version: 2020_11_22_122343) do
 
   create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "school_id"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 2020_11_22_121442) do
     t.integer "class_id"
     t.string "student_id"
     t.string "invite_code"
+    t.string "auth_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
