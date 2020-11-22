0187286ff1565fdd922d2506f4a015638d7dd453
knskyni <knskyni@yanai-k.com>
2020-11-22 20:28:23 +0900

    feat: StudentCreateモデルのvalidatesを実装

--- app/models/student_create.rb
+++ app/models/student_create.rb
@@ -1,2 +1,5 @@
 class StudentCreate < ApplicationRecord
+  validates :auth_key, {presence: true, uniqueness: true}
+  validates :student_id, {presence: true, uniqueness: true, length: {is: 7}, format: {with: /\A[0-9]+\z/, message: "半角数字で入力して下さい"}}
+  validates :email, {presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP, message: "半角数字で入力して下さい"}}
 end
