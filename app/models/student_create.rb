c11fa29352d649944c3898646e63cab0dd77ae62
yanakence <yanakence@yanah.jp>
2020-11-22 20:14:21 +0900

    feat: student_createテーブルを作成

---
+++ app/models/student_create.rb
@@ -0,0 +1,2 @@
+class StudentCreate < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_030508) do
+ActiveRecord::Schema.define(version: 2020_11_22_111249) do
 
   create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "school_id"
@@ -120,6 +120,14 @@ ActiveRecord::Schema.define(version: 2020_11_18_030508) do
 
   create_table "schools", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "student_creates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "auth_key"
+    t.string "student_id"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
