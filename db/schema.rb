8e0255e376f57235cdc0baedb0aa1e67dcf47bed
yanakence <yanakence@yanah.jp>
2020-11-23 01:17:12 +0900

    feat: school_classesテーブルにinvite_codeカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_122343) do
+ActiveRecord::Schema.define(version: 2020_11_22_161554) do
 
   create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "school_id"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2020_11_22_122343) do
     t.integer "course_id"
     t.string "school_class"
     t.boolean "graduated", default: false
+    t.string "invite_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
