1c16b05c96a05e2ac6ec06c168f308ae42a0876d
yanakence <yanakence@yanah.jp>
2020-11-23 17:27:12 +0900

    refactor: StudentCreateモデルをリファクタ

--- app/models/student_create.rb
+++ app/models/student_create.rb
@@ -1,19 +1,20 @@
 class StudentCreate < ApplicationRecord
-  validate :check_create
-
+  validate :exist_student, :check_invite_code
   validates :student_id, {presence: true, uniqueness: true, length: {is: 7}, format: {with: /\A[0-9]+\z/, message: "半角数字で入力して下さい"}}
   validates :invite_code, {presence: true}
   validates :auth_key, {uniqueness: true, allow_nil: true}
 
-  def check_create
-    school_class = SchoolClass.find_by(invite_code: self.invite_code)
-    if school_class.nil?
-      errors.add(:invite_code, "招待コードが有効ではありません。")
+  def exist_student
+    if Student.find_by(id: self.student_id)
+      errors.add(:student_id, "この学籍番号は既に本登録をしています。")
     end
+  end
 
-    student = Student.find_by(id: self.student_id)
-    if student
-      errors.add(:student_id, "この学籍番号は既に登録済みです。")
+  def check_invite_code
+    if SchoolClass.find_by(invite_code: self.invite_code).nil?
+      errors.add(:invite_code, "この招待コードは存在しません。")
+    else
+      self.class_id = school_class.id
     end
   end
 end
