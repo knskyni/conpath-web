74c6b5983827ee69b6cfcc79fce2fcbcd6a55fa9
MitaDan <kit.nakakai25@gmail.com>
2020-11-25 01:48:00 +0900

    feat: 企業情報修正機能の実装

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -64,6 +64,26 @@ class CompanyController < ApplicationController
     @recruit_company = RecruitCompany.find_by(id: params[:id])
   end
   def update
-    redirect_to("/company/:id")
+    @recruit_company = RecruitCompany.find_by(id: params[:id])
+    @recruit_company.company_code = params[:company_code]
+    @recruit_company.name = params[:name]
+    @recruit_company.name_furigana = params[:name_furigana]
+    @recruit_company.postal_code = params[:postal_code]
+    @recruit_company.prefecture_id = params[:prefecture_id]
+    @recruit_company.address = params[:address]
+    @recruit_company.found_date = params[:found_date]
+    @recruit_company.tel_number = params[:tel_number]
+    @recruit_company.fax_number = params[:fax_number]
+    @recruit_company.stock_list = params[:stock_list]
+    @recruit_company.number_of_employee_male = params[:number_of_employee_male]
+    @recruit_company.number_of_employee_female = params[:number_of_employee_female]
+    @recruit_company.number_of_employee = params[:number_of_employee]
+    @recruit_company.capital = params[:capital]
+    @recruit_company.proceed = params[:proceed]
+    @recruit_company.bisiness_details = params[:bisiness_details]
+    @recruit_company.url = params[:url]
+    @recruit_company.recruit_url = params[:recruit_url]
+    @recruit_company.save
+    redirect_to("/company/#{@recruit_company.id}")
   end
 end
