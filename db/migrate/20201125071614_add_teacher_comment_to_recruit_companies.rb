2da751ec09434188ad802898abea8868ffd00684
MitaDan <kit.nakakai25@gmail.com>
2020-11-25 16:32:46 +0900

    feat: recruit_companyテーブルにteacher_commentカラムを追加

--- db/migrate/20201125071614_add_teacher_comment_to_recruit_companies.rb
+++ db/migrate/20201125071614_add_teacher_comment_to_recruit_companies.rb
@@ -1,5 +1,5 @@
 class AddTeacherCommentToRecruitCompanies < ActiveRecord::Migration[5.2]
   def change
-    add_column :recruit_companies, :teacher_comment, :string
+    add_column :recruit_companies, :teacher_comment, :string, after: :recruit_url
   end
 end
