04b4f884d496ef8f00ac092b18538ef8e1e370ad
MitaDan <kit.nakakai25@gmail.com>
2020-11-25 16:32:46 +0900

    feat: recruit_companyテーブルにteacher_commentカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_065002) do
+ActiveRecord::Schema.define(version: 2020_11_25_071614) do
 
   create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "school_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_065002) do
     t.text "bisiness_details"
     t.text "url"
     t.text "recruit_url"
+    t.string "teacher_comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
