32ee31390d902202d7fc5ad1170844c4d45eae7a
yanakence <yanakence@yanah.jp>
2020-11-25 16:30:26 +0900

    feat: 学科登録処理を実装

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -4,6 +4,17 @@ class CourseController < ApplicationController
   end
 
   def create
+    @course = Course.new(
+        school_id: params[:school_id],
+        name: params[:course][:name],
+        number_of_enrolled_year: params[:course][:number_of_enrolled_year]
+    )
 
+    if @course.save
+      flash[:notice] = "学科の登録に成功しました。"
+      redirect_to("/course/#{@course.id}")
+    else
+      render("course/new")
+    end
   end
 end
