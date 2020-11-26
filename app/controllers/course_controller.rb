728eb2f4875dbdeac5b64d400f168e7ec645efe5
yanakence <yanakence@yanah.jp>
2020-11-26 12:46:43 +0900

    feat: 学科更新処理を実装した

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -21,4 +21,21 @@ class CourseController < ApplicationController
   def show
     @course = Course.find_by(id: params[:id])
   end
+
+  def edit
+    @course = Course.find_by(id: params[:id])
+  end
+
+  def update
+    @course = Course.find_by(id: params[:id])
+    @course.name = params[:course][:name]
+    @course.number_of_enrolled_year = params[:course][:number_of_enrolled_year]
+
+    if @course.save
+      flash[:notice] = "学科情報の更新に成功しました。"
+      redirect_to("/course/#{@course.id}")
+    else
+      render("course/edit")
+    end
+  end
 end
