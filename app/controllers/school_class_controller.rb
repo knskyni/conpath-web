59311b075c460ca63fa249bb709272429cc489b4
knskyni <knskyni@yanai-k.com>
2020-11-27 08:45:12 +0900

    feat: クラス更新ページを実装

--- app/controllers/school_class_controller.rb
+++ app/controllers/school_class_controller.rb
@@ -34,10 +34,23 @@ class SchoolClassController < ApplicationController
   end
 
   def edit
-
+    @school_class = SchoolClass.find_by(id: params[:id])
+    render(file: "#{Rails.root}/public/404", layout: false, status: :not_found) if @school_class.nil?
   end
 
   def update
+    @school_class = SchoolClass.find_by(id: params[:id])
+    render(file: "#{Rails.root}/public/404", layout: false, status: :not_found) if @school_class.nil?
 
+    @school_class.begin_year = params[:school_class][:begin_year]
+    @school_class.school_class = params[:school_class][:school_class]
+    @school_class.graduated = params[:school_class][:graduated]
+
+    if @school_class.save
+      flash[:notice] = "クラスの登録に成功しました。"
+      redirect_to("/class/#{@school_class.id}")
+    else
+      render("school_class/edit")
+    end
   end
 end
