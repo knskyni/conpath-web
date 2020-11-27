732f7305df720b529d817f0d14da83b526219c10
ikeguchi <ikenana619@gmail.com>
2020-11-27 19:05:44 +0900

    feat: Recruit_Postsコントローラのnewアクションの実装途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'recruit/post/new', to: 'recruit_post#new'
   root to: 'home#index'
-  get recruit/post/new => "posts#new"
-  post 'recruit/post/create' => 'posts#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'recruit_post/new', to: 'recruit_post#new'
+  post 'recruit_post/create', to: "posts#create"
+
 end
