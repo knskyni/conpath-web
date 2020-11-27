6e6d2d71d7f4411e39602ddbfadfa08fdc5ab471
ikeguchi <ikenana619@gmail.com>
2020-11-27 10:10:11 +0900

    fix: Teacherモデルの記述ミスを修正

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,11 +1,11 @@
 class Teacher < ApplicationRecord
   has_secure_password
 
-  validates :email, {presence: true, length: [maximum: 255]}
-  validates :last_name, {presence: true, length: [maximum: 255]}
-  validates :first_name, {presence: true,length: [maximum: 255]}
-  validates :last_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}, length: [maximum: 255]}
-  validates :first_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}, length: [maximum: 255]}
+  validates :email, {presence: true, length: {maximum: 255}}
+  validates :last_name, {presence: true, length: {maximum: 255}}
+  validates :first_name, {presence: true,length: {maximum: 255}}
+  validates :last_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}, length: {maximum: 255}}
+  validates :first_name_furigana, {presence: true, format: {with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力して下さい"}, length: {maximum: 255}}
   validates :gender, {presence: true}
-  validates :biography, {presence: false, length: [maximum: 255]}
+  validates :biography, {presence: false, length: {maximum: 255}}
 end
