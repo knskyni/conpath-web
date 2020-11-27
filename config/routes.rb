d18a23a3a110cb12bdea9a877dd053676c9ccb36
ikeguchi <ikenana619@gmail.com>
2020-11-27 16:24:56 +0900

    feat: 教員情報登録の文脈ミス修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   get 'teacher/new', to: 'teacher#new'
-  post 'teacher/new', to: 'teacher#create'
+  post 'teacher/create', to: 'teacher#create'
   get 'teacher/:id', to: 'teacher#show'
 end
