a3b69d1e17596f642765267bc01bc728db8f3f13
yanakence <yanakence@yanah.jp>
2020-11-27 10:48:49 +0900

    feat: ログイン前のトップページを実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,22 @@
 class HomeController < ApplicationController
   def index
-    @sub_header = {
-        title: "General",
-        list: [
-            {
-                name: "General",
-                url: "/"
-            },
-            {
-                name: "Empty Page",
-                url: "/test"
-            }
-        ]
-    }
+    if @current_user
+      @sub_header = {
+          title: "General",
+          list: [
+              {
+                  name: "General",
+                  url: "/"
+              },
+              {
+                  name: "Empty Page",
+                  url: "/test"
+              }
+          ]
+      }
+      render("home/index_after")
+    else
+      render("home/index_before", layout: false)
+    end
   end
 end
