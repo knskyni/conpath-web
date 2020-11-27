0cbca9d95be74696647334c50ef2ab3a0d39e8d0
ikeguchi <ikenana619@gmail.com>
2020-11-27 10:08:26 +0900

    feat: Teacherコントローラのshowアクションを実装した

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get 'teacher/new', to: 'teacher#new'
   post 'teacher/create', to: 'teacher#create'
+  get 'teacher/:id', to: 'teacher#show'
 end
