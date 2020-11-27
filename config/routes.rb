75511249b608d330567329bb16b19ca49e933ddf
ikeguchi <ikenana619@gmail.com>
2020-11-27 10:09:31 +0900

    feat: Teacherコントローラのnewアクションを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
   get 'teacher/new', to: 'teacher#new'
-  post 'teacher/create', to: 'teacher#create'
+  post 'teacher/new', to: 'teacher#create'
   get 'teacher/:id', to: 'teacher#show'
 end
