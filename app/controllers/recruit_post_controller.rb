829240cafe0625b3a86587f3dc60bba69962ce4e
ikeguchi <ikenana619@gmail.com>
2020-11-27 19:05:44 +0900

    feat: Recruit_Postsコントローラのnewアクションの実装途中

--- app/controllers/recruit_post_controller.rb
+++ app/controllers/recruit_post_controller.rb
@@ -1,8 +1,40 @@
 class RecruitPostController < ApplicationController
   def new
-    @recruit_posts = RecruitPost.new
-    @sub_header = {
-        titile: "求人票作成ページ"
-    }
+    @recruit_company = RecruitCompany.find_by(id: params[:company_id])
+    render(file: "#{Rails.root}/public/404", layout: false, status: :not_found) if @recruit_company.nil?
+
+    @recruit_posts = RecruitPost.new(
+        company_id: @company.id
+    )
+  end
+
+  def create
+    @recruit_company = RecruitCompany.find_by(id: params[:company_id])
+    render(file: "#{Rails.root}/public/404", layout: false, status: :not_found) if @recruit_company.nil?
+
+    @recruit_posts = RecruitPost.new(
+        recruit_code: params[:recruit_code],
+        company_id: @company.id,
+        year: params[:year],
+        job_category_id: params[:job_category_id],
+        job_description: params[:job_description],
+        recruitment_numbers: params[:recruitment_numbers],
+        working_office: params[:working_office],
+        status: params[:status],
+        employment_status: params[:employment_status],
+        employment_status_other: params[:employment_status_other],
+        salary_2year: params[:salary_2year],
+        salary_3year: params[:salary_3year],
+        salary_4year: params[:salary_4year],
+        apply_method: params[:apply_method],
+        submit_document: params[:submit_document],
+        other_description: params[:other_description]
+        )
+    if @recruit_posts.save
+      flash[:notice] = "求人情報の登録が完了いたしました"
+      redirect_to("/recruit_posts/#{@recruit_posts.id}")
+    else
+      render("recruit_posts/new")
+    end
   end
 end
