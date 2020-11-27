4830b03a51b412dc3b8b32821b7ecb2914529637
ikeguchi <ikenana619@gmail.com>
2020-11-27 14:12:49 +0900

    feat: Teacherコントローラのnewアクションの登録機能を実装(完成版)

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   get 'teacher/new', to: 'teacher#new'
-<<<<<<< 16517a33bfb5d964be6b773367967d3671b440b9
-  post 'teacher/create', to: 'teacher#create'
-=======
   post 'teacher/new', to: 'teacher#create'
   get 'teacher/:id', to: 'teacher#show'
 >>>>>>> feat: Teacherコントローラのnewアクションを実装
--- db/migrate/20201117012002_create_teachers.rb
+++ db/migrate/20201117012002_create_teachers.rb
@@ -2,7 +2,7 @@ class CreateTeachers < ActiveRecord::Migration[5.2]
   def change
     create_table :teachers do |t|
       t.string :email
-      t.string :password
+      t.string :password_digest
       t.string :last_name
       t.string :first_name
       t.string :last_name_furigana
