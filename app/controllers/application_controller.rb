43c8ed80647b3a8be44081d93aa9ceb3d2ad10f6
yanakence <yanakence@yanah.jp>
2020-12-01 20:05:33 +0900

    feat: サブヘッダーを各コントローラのスーパークラスで定義するようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate
+
+  def initialize
+    @sub_header = {
+        title: "タイトルなし",
+        list: []
+    }
+
+    super
+  end
+
   def authenticate
     if session[:user_type]
       if session[:user_type] == "student"
@@ -8,5 +18,13 @@ class ApplicationController < ActionController::Base
         @current_user = Teacher.find_by(id: session[:user_id])
       end
     end
+  end
+
+  def set_sub_header_title(title)
+    @sub_header[:title] = title
+  end
+
+  def add_sub_header_path(name, url)
+    @sub_header[:list].push({name: name, url: url})
   end
 end
