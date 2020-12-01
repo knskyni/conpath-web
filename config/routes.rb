98bd5c79a23277e35719e6bcda3d921e73209212
ikeguchi <ikenana619@gmail.com>
2020-12-01 15:57:01 +0900

    feat: recruit_postのnewアクションのルート名変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  get 'recruit_post/new', to: 'recruit_post#new'
-  post 'recruit_post/create', to: "posts#create"
+  get 'recruit_post/new/:company_id', to: 'recruit_post#new'
+  post 'recruit_post/create/:company_id', to: "recruit_post#create"
 
 end
