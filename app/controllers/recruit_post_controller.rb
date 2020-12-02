9915e8f596b2d42d0cd4b707afb58715febdeaa7
ikeguchi <ikenana619@gmail.com>
2020-12-02 15:56:28 +0900

    feat: recruit_postコントローラの細かな修正

--- app/controllers/recruit_post_controller.rb
+++ app/controllers/recruit_post_controller.rb
@@ -2,7 +2,7 @@ class RecruitPostController < ApplicationController
   def new
     # サブヘッダー
     set_sub_header_title("求人情報登録")
-    add_sub_header_path("求人票一覧", "/recruit_post/show/#{@recruit_posts.id}")
+    add_sub_header_path("求人票一覧", "/recruit_post/show/#{@recruit_post.id}")
     add_sub_header_path("登録", "/recruit_post/new/#{@recruit_company.id}")
     add_sub_header_path("編集", nil)
 
@@ -12,7 +12,7 @@ class RecruitPostController < ApplicationController
       return
     end
 
-    @recruit_posts = RecruitPost.new(
+    @recruit_post = RecruitPost.new(
         company_id: @recruit_company.id
     )
 
@@ -25,7 +25,7 @@ class RecruitPostController < ApplicationController
       return
     end
 
-    @recruit_posts = RecruitPost.new(
+    @recruit_post = RecruitPost.new(
         recruit_code: params[:recruit_code],
         company_id: @recruit_company.id,
         year: params[:year],
@@ -44,7 +44,7 @@ class RecruitPostController < ApplicationController
         other_description: params[:other_description]
         )
 
-    if @recruit_posts.save
+    if @recruit_post.save
       flash[:notice] = "求人情報の登録が完了いたしました"
       redirect_to("/")
     else
@@ -56,11 +56,11 @@ class RecruitPostController < ApplicationController
 
     # サブヘッダー
     set_sub_header_title("求人票一覧")
-    add_sub_header_path("求人情報", "/recruit_post/show/#{@recruit_posts.id}")
+    add_sub_header_path("求人情報", "/recruit_post/show/#{@recruit_post.id}")
     add_sub_header_path("登録", "/recruit_post/new/#{@recruit_company.id}")
     add_sub_header_path("編集", nil)
 
-    @recruit_posts = RecruitPost.find_by(id: params[:id])
+    @recruit_post = RecruitPost.find_by(id: params[:id])
     @recruit_company = RecruitCompany.find_by(id: params[:company_id])
 
   end
@@ -68,34 +68,34 @@ class RecruitPostController < ApplicationController
   def edit
 
     set_sub_header_title("求人情報編集")
-    add_sub_header_path("求人票一覧", "/recruit_post/show/#{@recruit_posts.id}")
+    add_sub_header_path("求人票一覧", "/recruit_post/show/#{@recruit_post.id}")
     add_sub_header_path("登録", "/recruit_post/new/#{@recruit_company.id}")
     add_sub_header_path("編集", nil)
 
-    @recruit_posts = RecruitPost.find_by(id: params[:id])
+    @recruit_post = RecruitPost.find_by(id: params[:id])
   end
 
   def update
 
-    @recruit_posts = RecruitPost.find_by(id: params[:id])
+    @recruit_post = RecruitPost.find_by(id: params[:id])
 
-    @recruit_posts.recruit_code = params[:recruit_code]
-    @recruit_posts.year = params[:year]
-    @recruit_posts.job_category_id = params[:job_category_id]
-    @recruit_posts.job_description = params[:job_description]
-    @recruit_posts.recruitment_numbers = params[:recruitment_numbers]
-    @recruit_posts.working_office = params[:working_office]
-    @recruit_posts.status = params[:status]
-    @recruit_posts.employment_status = params[:employment_status]
-    @recruit_posts.employment_status_other = params[:employment_status_other]
-    @recruit_posts.salary_2year = params[:salary_2year]
-    @recruit_posts.salary_3year = params[:salary_3year]
-    @recruit_posts.salary_4year = params[:salary_4year]
-    @recruit_posts.apply_method = params[:apply_method]
-    @recruit_posts.submit_document = params[:submit_document]
-    @recruit_posts.other_description = params[:other_description]
+    @recruit_post.recruit_code = params[:recruit_code]
+    @recruit_post.year = params[:year]
+    @recruit_post.job_category_id = params[:job_category_id]
+    @recruit_post.job_description = params[:job_description]
+    @recruit_post.recruitment_numbers = params[:recruitment_numbers]
+    @recruit_post.working_office = params[:working_office]
+    @recruit_post.status = params[:status]
+    @recruit_post.employment_status = params[:employment_status]
+    @recruit_post.employment_status_other = params[:employment_status_other]
+    @recruit_post.salary_2year = params[:salary_2year]
+    @recruit_post.salary_3year = params[:salary_3year]
+    @recruit_post.salary_4year = params[:salary_4year]
+    @recruit_post.apply_method = params[:apply_method]
+    @recruit_post.submit_document = params[:submit_document]
+    @recruit_post.other_description = params[:other_description]
 
-    if @recruit_posts.save
+    if @recruit_post.save
       flash[:notice] = "教員情報の変更を完了いたしました"
       redirect_to("/")
     else
