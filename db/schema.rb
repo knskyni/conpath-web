7d5b8f972854c59c45c959196538206de2045460
knskyni <knskyni@yanai-k.com>
2020-12-02 12:45:32 +0900

    feat: RecruitFavoriteStudentIndustryモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_133803) do
+ActiveRecord::Schema.define(version: 2020_12_02_034455) do
 
   create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "school_id"
@@ -73,6 +73,13 @@ ActiveRecord::Schema.define(version: 2020_11_26_133803) do
 
   create_table "recruit_company_tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recruit_favorite_student_industries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "student_id"
+    t.integer "industry_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
