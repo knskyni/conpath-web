736ea0d96aa45297b00a570290f4d66554d202b6
yanakence <yanakence@yanah.jp>
2020-12-03 14:52:02 +0900

    feat: クラスページにサブヘッダーを実装

--- app/controllers/school_class_controller.rb
+++ app/controllers/school_class_controller.rb
@@ -6,9 +6,13 @@ class SchoolClassController < ApplicationController
       return
     end
 
-    @school_class = SchoolClass.new(
-        course_id: @course.id
-    )
+    @school_class = SchoolClass.new(course_id: @course.id)
+
+    # サブヘッダー
+    set_sub_header_title("クラス登録")
+    add_sub_header_path(@school_class.course.school.name, "/school/#{@course.school.id}")
+    add_sub_header_path(@school_class.course.name, "/course/#{@course.id}")
+    add_sub_header_path("クラス登録", nil)
   end
 
   def create
@@ -30,6 +34,12 @@ class SchoolClassController < ApplicationController
       flash[:notice] = "クラスの登録に成功しました。"
       redirect_to("/class/#{@school_class.id}")
     else
+      # サブヘッダー
+      set_sub_header_title("クラス登録")
+      add_sub_header_path(@school_class.course.school.name, "/school/#{@school_class.course.school.id}")
+      add_sub_header_path(@school_class.course.name, "/course/#{@school_class.course.id}")
+      add_sub_header_path("クラス登録", nil)
+
       render("school_class/new")
     end
   end
@@ -40,6 +50,12 @@ class SchoolClassController < ApplicationController
       render_404
       return
     end
+
+    # サブヘッダー
+    set_sub_header_title(@school_class.name)
+    add_sub_header_path(@school_class.course.school.name, "/school/#{@school_class.course.school.id}")
+    add_sub_header_path(@school_class.course.name, "/course/#{@school_class.course.id}")
+    add_sub_header_path("詳細", nil)
   end
 
   def edit
@@ -48,6 +64,13 @@ class SchoolClassController < ApplicationController
       render_404
       return
     end
+
+    # サブヘッダー
+    set_sub_header_title("クラス情報編集")
+    add_sub_header_path(@school_class.course.school.name, "/school/#{@school_class.course.school.id}")
+    add_sub_header_path(@school_class.course.name, "/course/#{@school_class.course.id}")
+    add_sub_header_path(@school_class.name, "/class/#{@school_class.id}")
+    add_sub_header_path("編集", nil)
   end
 
   def update
@@ -65,6 +88,12 @@ class SchoolClassController < ApplicationController
       flash[:notice] = "クラスの登録に成功しました。"
       redirect_to("/class/#{@school_class.id}")
     else
+      set_sub_header_title("クラス情報編集")
+      add_sub_header_path(@school_class.course.school.name, "/school/#{@school_class.course.school.id}")
+      add_sub_header_path(@school_class.course.name, "/course/#{@school_class.course.id}")
+      add_sub_header_path(@school_class.name, "/class/#{@school_class.id}")
+      add_sub_header_path("編集", nil)
+
       render("school_class/edit")
     end
   end
