b02882b649d1675c973d9595cd139227ffd78aef
knskyni <knskyni@yanai-k.com>
2020-12-09 14:53:26 +0900

    refactor: PostコントローラがRecruitPostコントローラになっていたのを修正

--- test/controllers/recruit_post_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class RecruitPostControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get recruit_post_new_url
-    assert_response :success
-  end
-
-end
