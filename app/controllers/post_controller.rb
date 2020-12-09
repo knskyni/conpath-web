62b8b2c13ebb9fd7b5198c0967abf83a264a5821
knskyni <knskyni@yanai-k.com>
2020-12-09 15:49:37 +0900

    refactor: 変数名を簡略化した

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,15 +1,15 @@
 class PostController < ApplicationController
   def new
     # URLの企業IDから企業情報を取得
-    @recruit_company = RecruitCompany.find_by(id: params[:company_id])
+    @company = RecruitCompany.find_by(id: params[:company_id])
 
     # 企業情報取得できなければ404
-    render_404 and return if @recruit_company.nil?
+    render_404 and return if @company.nil?
 
     # view用
     @job_categories = RecruitJobCategory.all
-    @recruit_post = RecruitPost.new(
-        company_id: @recruit_company.id
+    @post = RecruitPost.new(
+        company_id: @company.id
     )
 
     # サブヘッダー
@@ -20,15 +20,15 @@ class PostController < ApplicationController
 
   def create
     # URLの企業IDから企業情報を取得
-    @recruit_company = RecruitCompany.find_by(id: params[:company_id])
+    @company = RecruitCompany.find_by(id: params[:company_id])
 
     # 企業情報取得できなければ404
-    render_404 and return if @recruit_company.nil?
+    render_404 and return if @company.nil?
 
     # view用
-    @recruit_post = RecruitPost.new(
+    @post = RecruitPost.new(
         recruit_code: params[:recruit_code],
-        company_id: @recruit_company.id,
+        company_id: @company.id,
         year: params[:year],
         job_category_id: params[:job_category_id],
         job_description: params[:job_description],
@@ -45,9 +45,9 @@ class PostController < ApplicationController
         other_description: params[:other_description]
     )
 
-    if @recruit_post.save
+    if @post.save
       flash[:notice] = "求人票の登録が完了しました。"
-      redirect_to("/post/#{@recruit_post.id}")
+      redirect_to("/post/#{@post.id}")
     else
       # サブヘッダー
       set_sub_header_title("求人票新規登録")
@@ -60,65 +60,65 @@ class PostController < ApplicationController
 
   def show
     # URLの求人票IDから求人票情報を取得
-    @recruit_post = RecruitPost.find_by(id: params[:id])
+    @post = RecruitPost.find_by(id: params[:id])
 
     # 求人票情報取得できなければ404
-    render_404 and return if @recruit_post.nil?
+    render_404 and return if @post.nil?
 
     # サブヘッダー
-    set_sub_header_title("#{@recruit_post.company.name}(#{@recruit_post.year}年)")
+    set_sub_header_title("#{@post.company.name}(#{@post.year}年)")
     add_sub_header_path("求人票", "/post")
-    add_sub_header_path("#{@recruit_post.company.name}(#{@recruit_post.year}年)", nil)
+    add_sub_header_path("#{@post.company.name}(#{@post.year}年)", nil)
   end
 
   def edit
     # URLの求人票IDから求人票情報を取得
-    @recruit_post = RecruitPost.find_by(id: params[:id])
+    @post = RecruitPost.find_by(id: params[:id])
 
     # 求人票情報取得できなければ404
-    render_404 and return if @recruit_post.nil?
+    render_404 and return if @post.nil?
 
     # サブヘッダー
-    set_sub_header_title("編集: #{@recruit_post.company.name}(#{@recruit_post.year}年)")
+    set_sub_header_title("編集: #{@post.company.name}(#{@post.year}年)")
     add_sub_header_path("求人票", "/post")
-    add_sub_header_path("#{@recruit_post.company.name}(#{@recruit_post.year}年)", "/post/#{@recruit_post.id}")
+    add_sub_header_path("#{@post.company.name}(#{@post.year}年)", "/post/#{@post.id}")
     add_sub_header_path("編集", nil)
   end
 
   def update
     # URLの求人票IDから求人票情報を取得
-    @recruit_post = RecruitPost.find_by(id: params[:id])
+    @post = RecruitPost.find_by(id: params[:id])
 
     # 求人票情報取得できなければ404
-    render_404 and return if @recruit_post.nil?
+    render_404 and return if @post.nil?
 
     # フォームデータを挿入
-    @recruit_post.recruit_code = params[:recruit_code]
-    @recruit_post.year = params[:year]
-    @recruit_post.job_category_id = params[:job_category_id]
-    @recruit_post.job_description = params[:job_description]
-    @recruit_post.recruitment_numbers = params[:recruitment_numbers]
-    @recruit_post.working_office = params[:working_office]
-    @recruit_post.status = params[:status]
-    @recruit_post.employment_status = params[:employment_status]
-    @recruit_post.employment_status_other = params[:employment_status_other]
-    @recruit_post.salary_2year = params[:salary_2year]
-    @recruit_post.salary_3year = params[:salary_3year]
-    @recruit_post.salary_4year = params[:salary_4year]
-    @recruit_post.apply_method = params[:apply_method]
-    @recruit_post.submit_document = params[:submit_document]
-    @recruit_post.other_description = params[:other_description]
+    @post.recruit_code = params[:recruit_code]
+    @post.year = params[:year]
+    @post.job_category_id = params[:job_category_id]
+    @post.job_description = params[:job_description]
+    @post.recruitment_numbers = params[:recruitment_numbers]
+    @post.working_office = params[:working_office]
+    @post.status = params[:status]
+    @post.employment_status = params[:employment_status]
+    @post.employment_status_other = params[:employment_status_other]
+    @post.salary_2year = params[:salary_2year]
+    @post.salary_3year = params[:salary_3year]
+    @post.salary_4year = params[:salary_4year]
+    @post.apply_method = params[:apply_method]
+    @post.submit_document = params[:submit_document]
+    @post.other_description = params[:other_description]
 
-    if @recruit_post.save
+    if @post.save
       flash[:notice] = "教員情報の変更を完了いたしました"
       redirect_to("/")
     else
       # サブヘッダー
-      set_sub_header_title("編集: #{@recruit_post.company.name}(#{@recruit_post.year}年)")
+      set_sub_header_title("編集: #{@post.company.name}(#{@post.year}年)")
       add_sub_header_path("求人票", "/post")
-      add_sub_header_path("#{@recruit_post.company.name}(#{@recruit_post.year}年)", "/post/#{@recruit_post.id}")
+      add_sub_header_path("#{@post.company.name}(#{@post.year}年)", "/post/#{@post.id}")
       add_sub_header_path("編集", nil)
-      
+
       render("post/edit")
     end
   end
