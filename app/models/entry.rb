c93004a223c43265d01f6f6355489f48ca907a19
knskyni <knskyni@yanai-k.com>
2020-12-13 21:36:00 +0900

    feat: Entryモデルにvalidatesを追加

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,9 @@
 class Entry < ApplicationRecord
   belongs_to :student, foreign_key: :student_id
   belongs_to :recruit_company, foreign_key: :company_id
+
+  validates :student_id, {presence: true, uniqueness: {scope: :company_id}}
+  validates :company_id, {presence: true}
+  validates :status, {presence: true}
+  validates :start_date, {presence: true}
 end
