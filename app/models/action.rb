238e7a23a7123137a5681046db1ac41f9453eb84
yanakence <yanakence@yanah.jp>
2020-12-14 23:10:26 +0900

    feat: Actionモデルにvalidatesを実装

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,3 +1,7 @@
 class Action < ApplicationRecord
   belongs_to :entry, foreign_key: :entry_id
+
+  validates :entry_id, {presence: true}
+  validates :title, {presence: true}
+  validates :date, {presence: true}
 end
