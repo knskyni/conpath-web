fe3451e6e0070b4a46f2f0c0dac404005ea70090
yanakence <yanakence@yanah.jp>
2020-12-14 23:08:19 +0900

    feat: actionモデルの外部キー制約を実装

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,6 +2,8 @@ class Entry < ApplicationRecord
   belongs_to :student, foreign_key: :student_id
   belongs_to :recruit_post, foreign_key: :post_id
 
+  has_many :actions, foreign_key: :entry_id
+
   validates :student_id, {presence: true, uniqueness: {scope: :post_id}}
   validates :post_id, {presence: true}
   validates :status, {presence: true}
