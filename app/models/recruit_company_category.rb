97ef463a721288efc24cedc1457aefcc2ee64ef2
yanakence <yanakence@yanah.jp>
2020-12-16 02:07:33 +0900

    feat: 業種と業種割り当ての外部キー制約を実装

--- app/models/recruit_company_category.rb
+++ app/models/recruit_company_category.rb
@@ -1,3 +1,5 @@
 class RecruitCompanyCategory < ApplicationRecord
+  has_many :recruit_company_category_assigns, foreign_key: :company_category_id
+
   validates :name, {presence: true}
 end
--- app/models/recruit_company_category_assign.rb
+++ app/models/recruit_company_category_assign.rb
@@ -1,5 +1,6 @@
 class RecruitCompanyCategoryAssign < ApplicationRecord
   belongs_to :recruit_company, foreign_key: :company_id
+  belongs_to :recruit_company_category, foreign_key: :company_category_id
 
   validates :company_id, {presence: true}
   validates :company_category_id, {presence: true}
